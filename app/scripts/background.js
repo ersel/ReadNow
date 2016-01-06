37cfffa17ff5db90efe3addbac3bbbae795ead2f
Ersel Aker <erselaker@gmail.com>
2016-01-06 05:27:24 +0200

    event listeners to keep track of bookmarks to read count...

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,7 +4,19 @@ chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: 'Read'});
+function updateBadge() {
+  chrome.browserAction.setBadgeText({text: '#0'});
+  chrome.bookmarks.search('ReadNowBookmarks',
+  	function(BookmarksArray){
+  		if(BookmarksArray.length > 0){
+  			var nodeId = BookmarksArray[0].id;
+  			chrome.bookmarks.getChildren(nodeId, function(children){
+          chrome.browserAction.setBadgeText({text: '#' + children.length });
+        });
+      }
+    });
+}
 
-
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+chrome.bookmarks.onCreated.addListener(updateBadge);
+chrome.bookmarks.onRemoved.addListener(updateBadge);
+updateBadge();
